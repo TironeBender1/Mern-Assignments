62727207c1cf57b93bf51db676e58b7f248a0fff
TironeBender1 <108382896+TironeBender1@users.noreply.github.com>
2022-09-04 23:36:51 -0700

    Add files via upload

---
+++ destructuring.js
@@ -0,0 +1,97 @@
+// PROBLEM 1 
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// EXPECTED OUTPUT 
+// => Tesla
+// => Mercedes 
+
+
+
+
+// PROBLEM 2 
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+// EXPECTED OUTPUT 
+// ERROR: Name is not defined
+
+
+
+
+
+// PROBLEM 3
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+// EXPECTED OUTPUT
+// '12345'
+//  undefined
+
+
+
+
+
+// PROBLEM 4
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+copy
+
+
+// EXPECTED OUTPUT
+// false 
+// true 
+
+
+
+
+
+// PROBLEM 5
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// EXPECTED OUTPUT 
+// value
+// [1, 5, 1, 8, 3, 3]
+// 1
+// 5
+// 5
